dc27032dcf26c8006e62f968298ec1ee84ea9d66
Matt <marswebb@qq.com>
2016-12-08 20:49:46 +0800

    add Navigation Test

---
+++ GNavTest2ByS3/GNavTest2ByS3/NameViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NameViewController.swift
+//  GNavTest2ByS3
+//
+//  Created by 花生 on 8/12/2016.
+//  Copyright © 2016年 ihuasheng. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+    
+    var tempName: String = ""
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Chosen Name"
+        // Do any additional setup after loading the view.
+        nameLabel.text = tempName
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
