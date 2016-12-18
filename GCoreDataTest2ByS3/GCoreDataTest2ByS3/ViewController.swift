8f844b3b686ebba3580926bf228bc9acb7fdd0e8
Matt <marswebb@qq.com>
2016-12-18 21:22:29 +0800

    Modify the coredata test2

--- GCoreDataTest2ByS3/GCoreDataTest2ByS3/ViewController.swift
+++ GCoreDataTest2ByS3/GCoreDataTest2ByS3/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     private let entityName = "Students"
     private let nameColumn = "sName"
     private let ageColumn = "sAge"
+    private let noColumn = "sNo"
 
     func getContext() -> NSManagedObjectContext {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -32,14 +33,17 @@ class ViewController: UIViewController {
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
         do {
             let searchResults = try context.fetch(request)
+            var tempNo: Int32
             var tempAge: Int16
-            for (i, itemResult) in (searchResults as! [NSManagedObject]).enumerated() {
+            //for (i, itemResult) in (searchResults as! [NSManagedObject]).enumerated() {
+            for itemResult in searchResults as! [NSManagedObject] {
+                tempNo = itemResult.value(forKey: noColumn) as! Int32
                 tempAge = itemResult.value(forKey: ageColumn) as! Int16
-                if i == 0 {
+                if tempNo == 1 {
                     name1Text.text = itemResult.value(forKey: nameColumn) as? String
                     age1Text.text = String(tempAge)
                 }
-                if i == 1 {
+                if tempNo == 2 {
                     name2Text.text = itemResult.value(forKey: nameColumn) as? String
                     age2Text.text = String(tempAge)
                 }
@@ -56,24 +60,47 @@ class ViewController: UIViewController {
 
     @IBAction func saveStu(_ sender: Any) {
         let context = getContext()
-        // TODO Update NOT Create!!!!!!!!!!!
-        //let entity = NSEntityDescription.entity(forEntityName: entityName, in: context)
-        //let stuObj1 = NSManagedObject(entity: entity!, insertInto: context)
-        //var tempInt = Int16(age1Text.text!)!
-        //print(tempInt)
-        //stuObj1.setValue(name1Text.text, forKey: nameColumn)
-        //stuObj1.setValue(tempInt, forKey: ageColumn)
-        //let stuObj2 = NSManagedObject(entity: entity!, insertInto: context)
-        //tempInt = Int16(age2Text.text!)!
-        //stuObj2.setValue(name2Text.text, forKey: nameColumn)
-        //stuObj2.setValue(tempInt, forKey: ageColumn)
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        var resultText = ""
         do {
+            let searchResults = try context.fetch(request)
+            if searchResults.count == 0 {
+                //CREATE
+                let entity = NSEntityDescription.entity(forEntityName: entityName, in: context)
+                let stuObj1 = NSManagedObject(entity: entity!, insertInto: context)
+                var tempInt = Int16(age1Text.text!)!
+                stuObj1.setValue(name1Text.text, forKey: nameColumn)
+                stuObj1.setValue(tempInt, forKey: ageColumn)
+                stuObj1.setValue(1, forKey: noColumn)
+                let stuObj2 = NSManagedObject(entity: entity!, insertInto: context)
+                tempInt = Int16(age2Text.text!)!
+                stuObj2.setValue(name2Text.text, forKey: nameColumn)
+                stuObj2.setValue(tempInt, forKey: ageColumn)
+                stuObj2.setValue(2, forKey: noColumn)
+                resultText = "Done"
+            } else if searchResults.count == 2 {
+                //UPDATE
+                var tempNo: Int32
+                for itemResult in searchResults as! [NSManagedObject] {
+                    tempNo = itemResult.value(forKey: noColumn) as! Int32
+                    if tempNo == 1 {
+                        itemResult.setValue(name1Text.text, forKey: nameColumn)
+                        itemResult.setValue(Int16(age1Text.text!), forKey: ageColumn)
+                    }
+                    if tempNo == 2 {
+                        itemResult.setValue(name2Text.text, forKey: nameColumn)
+                        itemResult.setValue(Int16(age2Text.text!), forKey: ageColumn)
+                    }
+                }
+                resultText = "Done"
+            } else {
+                resultText = "Search Error"
+            }
             try context.save()
-            saveTips.text = "Done"
+            saveTips.text = resultText
         } catch {
             saveTips.text = "Save Error"
         }
     }
-
 }
 
