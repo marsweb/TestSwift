9081232043566cd53c75d9161a04ed1ff88e0f8a
Matt <marswebb@qq.com>
2017-03-31 11:05:01 +0800

    add new Menu Test

---
+++ GMenuTest1ByS3/GMenuTest1ByS3/LeftVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeftVC.swift
+//  GMenuTest1ByS3
+//
+//  Created by 花生 on 17/3/31.
+//  Copyright © 2017年 ihuasheng. All rights reserved.
+//
+
+import UIKit
+
+class LeftVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GMenuTest1ByS3/GMenuTest1ByS3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMenuTest1ByS3
+//
+//  Created by 花生 on 17/3/31.
+//  Copyright © 2017年 ihuasheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
